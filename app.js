d99c34835203be11bfa99e1afc30d2f4f1eb9380
Jeffar <jeffarmanurung66@gmail.com>
2023-02-14 14:58:59 +0700

    add Dockerfile

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use("/", (res) => {
+  res.statusCode(200).json({
+    success: "success",
+  });
+});
 app.use("/activity-groups", ActivityRouter);
 app.use("/todo-items", TodosRouter);
 
 const PORT = 3030;
 
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
   console.log(`app running on PORT https://localhost:${PORT}`);
+  if (err) {
+    console.log(err);
+  }
 });
--- database/connection.js
+++ database/connection.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-  if (err) throw error;
+  if (err) console.log(err);
   console.log("Mysql Connected");
 });
 
